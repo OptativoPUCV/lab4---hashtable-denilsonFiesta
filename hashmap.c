98a2b67c410d234940618597abe0dabaf8933926
Replit user <>
2023-04-10 23:33:14 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -88,14 +88,14 @@ void eraseMap(HashMap * map,  char * key) {
 
   if(is_equal(key, (*(map->buckets+pos))->key)){
     map->current =  pos;
-    *(map->buckets+pos))->key = NULL;
+    (*(map->buckets+pos))->key = NULL;
   }else{
 
     for(long i = 0, it = pos+1; i < map->capacity; i++, it++){
       it = it%map->capacity;
       if(is_equal(key, (*(map->buckets+it))->key)){
         map->current = it;
-        *(map->buckets+it))->key = NULL;
+        (*(map->buckets+it))->key = NULL;
       }
     }
     
