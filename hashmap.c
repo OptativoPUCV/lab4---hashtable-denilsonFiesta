01746ba06b51ed14193507692ac36a746d8cce8d
Replit user <>
2023-04-24 23:23:50 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -130,9 +130,9 @@ Pair * searchMap(HashMap * map,  char * key) {
 
   long pos = hash(key, map->capacity);
 
-  if(is_equal(key, (*(map->buckets+pos))->key)){
+  if(is_equal(map->buckets[pos]->key, key){
     map->current =  pos;
-    return *(map->buckets+pos);
+    return map->buckets+pos;
   }else{
 
     for(long i = 0, it = pos+1; i < map->capacity; i++, it++){
